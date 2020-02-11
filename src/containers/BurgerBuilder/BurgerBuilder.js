af14e023eceb99853c1d6fdd6e85089fa5b39ca2
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-02-11 15:10:35 +0000

    removed button now recognises no ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -41,6 +41,9 @@ this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
 
 removeIngredientHandler = (type) => {
 const oldCount = this.state.ingredients[type];
+if (oldCount <= 0) {
+    return;
+}
 const updatedCounted = oldCount - 1;
 const updatedIngredients = {
     ...this.state.ingredients
@@ -56,12 +59,19 @@ const oldprice = this.state.totalprice;
 
 
     render () {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+            for (let key in disabledInfo) {
+             disabledInfo[key] = disabledInfo[key] <= 0
+        }
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
-                    ingredientRemoved={this.removeIngredientHandler}/>
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo} />
             </Aux>
         );
     }
