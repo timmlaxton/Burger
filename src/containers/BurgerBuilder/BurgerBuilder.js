40a75b3c2323deedce393f88d1c9b894eb8217cb
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-02-11 14:52:00 +0000

    can remove items from order

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,7 +5,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 const INGREDIENT_PRICES = {
-    sald: 0.5,
+    salad: 0.5,
     cheese: 0.4,
     meat: 1.3,
     bacon: 2
@@ -36,18 +36,32 @@ const priceAddition = INGREDIENT_PRICES[type];
 const oldprice = this.state.totalprice;
 const newPrice =  oldprice + priceAddition;
 this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
-    }
 
-    removeIngredientHandler = (type) => {
+}
 
-    }
+removeIngredientHandler = (type) => {
+const oldCount = this.state.ingredients[type];
+const updatedCounted = oldCount - 1;
+const updatedIngredients = {
+    ...this.state.ingredients
+ };
+ updatedIngredients[type] = updatedCounted;
+ const priceDeduction = INGREDIENT_PRICES[type];
+const oldprice = this.state.totalprice;
+ const newPrice =  oldprice - priceDeduction;
+ this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+
+}   
+
+
 
     render () {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
-                    ingredientAdded={this.addIngredientHandler}/>
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}/>
             </Aux>
         );
     }
