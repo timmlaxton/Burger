ba9b45783bcd7d82588ae8c3824033651b33c30b
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-02-11 16:20:24 +0000

    added order button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,8 +21,20 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
 
+    }
+    updatePurchaseState (ingredients) {
+        
+         const sum = Object.keys(ingredients)
+         .map(igKey => {
+             return ingredients[igKey];
+         })
+         .reduce((sum, el) => {
+             return sum + el;
+         }, 0);
+         this.setState({purchasable: sum > 0});
     }
 
     addIngredientHandler = (type) => {
@@ -36,7 +48,7 @@ const priceAddition = INGREDIENT_PRICES[type];
 const oldPrice = this.state.totalPrice;
 const newPrice =  oldPrice + priceAddition;
 this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
-
+this.updatePurchaseState(updatedIngredients);
 }
 
 removeIngredientHandler = (type) => {
@@ -53,7 +65,7 @@ const updatedIngredients = {
  const oldPrice = this.state.totalPrice;
  const newPrice =  oldPrice - priceDeduction;
  this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
-
+ this.updatePurchaseState(updatedIngredients);
 }   
 
 
@@ -71,6 +83,7 @@ const updatedIngredients = {
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
+                    purchasable={this.state.purchasable}
                     disabled={disabledInfo} 
                     price={this.state.totalPrice}/>
             </Aux>
