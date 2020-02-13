ea2c74d445a795718162ad1f35a20f78722b38e8
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-02-13 13:54:30 +0000

    applied backdrop to sidebar

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,11 +3,18 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
-
+    let attachedClasses = [classes.SideDrawer, classes.Close]
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open]
+    }
     return (
-        <div className={classes.SideDrawer}>
+        <Aux>
+        <Backdrop show={props.open} clicked={props.closed}/>
+        <div className={attachedClasses.join(' ')}>
             <div className={classes.Logo}>
             <Logo />
             </div>
@@ -15,6 +22,7 @@ const sideDrawer = (props) => {
                 <NavigationItems />
             </nav>
         </div>
+        </Aux>
     );
 }
 
