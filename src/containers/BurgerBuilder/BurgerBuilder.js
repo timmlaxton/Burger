a2c349c172a612b1464cada68bb5ef626e301060
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-03-11 13:36:22 +0000

    added props.history to checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -98,29 +98,29 @@ purchaseCancelHandler = () => {
 
 purchaseContinueHandler = () => {
     //alert('You Continue');
-    this.setState({loading: true})
-    const order = {
-        ingredients: this.state.ingredients,
-        price: this.state.totalPrice,
-        cutomer: {
-            name: "tim",
-            address: {
-                city: "pigeon"
-            }
-        }
+    // this.setState({loading: true})
+    // const order = {
+    //     ingredients: this.state.ingredients,
+    //     price: this.state.totalPrice,
+    //     cutomer: {
+    //         name: "tim",
+    //         address: {
+    //             city: "pigeon"
+    //         }
+    //     }
         
-    }
+    // }
     
-    axios.post('/orders.json', order)
-    .then(response => {
-    this.setState({ loding: false, purchasing: false})
+    // axios.post('/orders.json', order)
+    // .then(response => {
+    // this.setState({ loding: false, purchasing: false})
     
-    })
+    // })
     
-    .catch( error => {
-        this.setState({ loding: false, purchasing: false})
-    });
-    
+    // .catch( error => {
+    //     this.setState({ loding: false, purchasing: false})
+    // });
+    this.props.history.push('/checkout')
 }
 
 
