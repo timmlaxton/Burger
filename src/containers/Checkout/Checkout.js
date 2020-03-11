86b715181235f2d500309834827c385500048033
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-03-11 11:24:16 +0000

    added checkout container

---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
+
+class Checkout extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            cheese: 1,
+            meat: 1,
+            bacon: 1,
+            pickles: 1,
+            chillies: 1,
+            tomatoes: 1,
+        }
+    }
+    render () {
+        return (
+            <div>
+                <CheckoutSummary ingredients={this.state.ingredients}/>
+            </div>
+        )
+    }
+
+}
+
+
+export default Checkout;
